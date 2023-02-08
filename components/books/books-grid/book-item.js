22dde4d5f1848171fb4465b8feee248b4fbe3fe0
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-08 19:22:32 +0000

    amended book-item.js to imgUrl

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,7 +1,7 @@
 export default function BookItem({ book }) {
   return (
     <div className="bg-red-100">
-      <img src={book.Imgurl} />
+      <img src={book.imgUrl} />
       <p>{book.title}</p>
       <p>{book.author}</p>
     </div>
