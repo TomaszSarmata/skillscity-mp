5f255e24a5511bcbc6c9e6128a49b983d380b886
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-08 19:18:35 +0000

    added images to the book  item

--- components/books/books-grid/book-item.js
+++ components/books/books-grid/book-item.js
@@ -1,6 +1,7 @@
 export default function BookItem({ book }) {
   return (
     <div className="bg-red-100">
+      <img src={book.Imgurl} />
       <p>{book.title}</p>
       <p>{book.author}</p>
     </div>
