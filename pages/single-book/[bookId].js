6195751e43a80bb1abe6de948293bc25c39c9e2e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-09 09:24:53 +0000

    added copy-to-clipboard component

--- pages/single-book/[bookId].js
+++ pages/single-book/[bookId].js
@@ -4,6 +4,7 @@ import Header from "@/components/shared/header";
 import Hero from "@/components/home/hero";
 import Footer from "@/components/shared/footer";
 import Content from "@/components/shared/content";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function SingleBook() {
   const router = useRouter();
@@ -48,6 +49,17 @@ export default function SingleBook() {
 
       <Content>
         <div className="w-full flex flex-col">
+          <div>
+            <CopyToClipboard text={book.linkToPurchase}>
+              <button
+                className="bg-blue-500 text-white px-4 py-2 rounded-lg"
+                type="button"
+              >
+                Copy link to Amazon
+              </button>
+            </CopyToClipboard>
+          </div>
+
           <a href={book.linkToPurchase} target="_blank">
             Buy on Amazon
           </a>
