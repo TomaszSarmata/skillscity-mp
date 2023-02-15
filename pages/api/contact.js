d4a2637e1d030850ebc2b5618c11e222c646c192
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-15 12:10:07 +0000

    created sanity check for contact-messages

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,6 +2,21 @@ import sql from "@/utils/postgres";
 export default async function handler(req, res) {
   const { name, email, message } = req.query;
 
+  if (name === undefined) {
+    res.status(400).json({ message: "Name is required " });
+    return;
+  }
+
+  if (email === undefined) {
+    res.status(400).json({ message: "Email is required " });
+    return;
+  }
+
+  if (message === undefined) {
+    res.status(400).json({ message: "Message is required " });
+    return;
+  }
+
   const response = await sql`
     insert into contact_messages
         (name, email, message)
