403e80fdacd48ce4bcec67e5306d5e1970154d84
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-15 12:10:39 +0000

    created api for contact-messages.js

---
+++ pages/api/contact-messages.js
@@ -0,0 +1,8 @@
+import sql from "@/utils/postgres";
+
+export default async function handler(req, res) {
+  const messages = await sql`
+        select * from contact_messages
+    `;
+  res.json({ messages: messages });
+}
