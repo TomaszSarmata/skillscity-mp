56dd9d3f89b5268f6f5bfac218ab1b2b683630c3
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-15 18:16:21 +0000

    brakpoint responsive colors for hero h1

--- components/home/hero/index.js
+++ components/home/hero/index.js
@@ -6,7 +6,9 @@ export default function Hero({ img_url, title, subtitle }) {
           <img src={img_url} className="rounded-lg" />
         </div>
         <div className="w-full text-center md:text-left">
-          <h1 className="text-6xl font-bold italic text-gray-700">{title}</h1>
+          <h1 className="text-6xl font-bold italic text-gray-700 sm:text-red-700 md:text-blue-400 lg:text-blue-700">
+            {title}
+          </h1>
           <p className="text-xl text-gray-700">{subtitle}</p>
         </div>
       </div>
