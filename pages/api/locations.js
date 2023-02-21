3c278618c96faad3673d699b83da14c2e3001f80
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-21 20:00:08 +0000

    added filtering to our endpoint and implemented the search button

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -1,8 +1,20 @@
 import sql from "@/utils/postgres";
 
 export default async function handler(req, res) {
-  const locations = await sql`
+  const search = req.query.search;
+
+  let locations = [];
+
+  if (search === null || search === undefined) {
+    locations = await sql`
+        select * from locations
+    `;
+  } else {
+    locations = await sql`
     select * from locations
-  `;
+    where title = ${search}
+    `;
+  }
+
   res.json(locations);
 }
