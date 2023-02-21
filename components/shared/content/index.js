22cb70d4f7a6f979bb02b73457d5bda7f4543373
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-02-21 16:39:45 +0000

    px-4 on content component

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,5 +1,5 @@
 import { Children } from "react";
 
 export default function Content({ children }) {
-  return <div className="w-full max-w-6xl mx-auto mt-5">{children}</div>;
+  return <div className="w-full max-w-6xl mx-auto mt-5 px-4">{children}</div>;
 }
